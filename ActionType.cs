71b1f23e1fa5814d11247f1b1911da580a5466bd
Kofi <kofi.turkson10@gmail.com>
2025-10-01 20:50:11 +0200

    Latest update

--- ActionType.cs
+++ ActionType.cs
@@ -6,3 +6,30 @@ public enum ActionType
     Block,
     Shotgun
 }
+public static class ActionConverter
+{
+    public static ActionType ConvertInput(string action)
+    {
+        if (action == "1")
+        {
+            return ActionType.Reload;
+        }
+        else if (action == "2")
+        {
+            return ActionType.Shoot;
+        }
+        else if (action == "3")
+        {
+            return ActionType.Block;
+        }
+
+        else if (action == "4")
+        {
+            return ActionType.Shotgun;
+        }
+        else
+        {
+            return ActionType.None;
+        }
+    }
+}
